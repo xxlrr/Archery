62f48955f21ea99c7b8d1593b8a7a48666174dc1
root <root@localhost.localdomain>
2020-03-18 15:58:21 +0800

    在邮件标题添加工单标题

--- sql/notify.py
+++ sql/notify.py
@@ -80,7 +80,7 @@ def notify_for_audit(audit_id, **kwargs):
 
     # 准备消息格式
     if status == WorkflowDict.workflow_status['audit_wait']:  # 申请阶段
-        msg_title = "[{}]新的工单申请#{}".format(workflow_type_display, audit_id)
+        msg_title = "[{}]新的工单申请#{}-{}".format(workflow_type_display, audit_id, workflow_title)
         # 接收人，发送给该资源组内对应权限组所有的用户
         auth_group_names = Group.objects.get(id=audit_detail.current_audit).name
         msg_to = auth_group_users([auth_group_names], audit_detail.group_id)
@@ -98,7 +98,7 @@ def notify_for_audit(audit_id, **kwargs):
             workflow_url,
             workflow_content)
     elif status == WorkflowDict.workflow_status['audit_success']:  # 审核通过
-        msg_title = "[{}]工单审核通过#{}".format(workflow_type_display, audit_id)
+        msg_title = "[{}]工单审核通过#{}-{}".format(workflow_type_display, audit_id, workflow_title)
         # 接收人，仅发送给申请人
         msg_to = [Users.objects.get(username=audit_detail.create_user)]
         msg_cc = Users.objects.filter(username__in=kwargs.get('cc_users', []))
@@ -114,7 +114,7 @@ def notify_for_audit(audit_id, **kwargs):
             workflow_url,
             workflow_content)
     elif status == WorkflowDict.workflow_status['audit_reject']:  # 审核驳回
-        msg_title = "[{}]工单被驳回#{}".format(workflow_type_display, audit_id)
+        msg_title = "[{}]工单被驳回#{}-{}".format(workflow_type_display, audit_id, workflow_title)
         # 接收人，仅发送给申请人
         msg_to = [Users.objects.get(username=audit_detail.create_user)]
         msg_cc = Users.objects.filter(username__in=kwargs.get('cc_users', []))
@@ -127,7 +127,7 @@ def notify_for_audit(audit_id, **kwargs):
             workflow_url,
             workflow_audit_remark)
     elif status == WorkflowDict.workflow_status['audit_abort']:  # 审核取消，通知所有审核人
-        msg_title = "[{}]提交人主动终止工单#{}".format(workflow_type_display, audit_id)
+        msg_title = "[{}]提交人主动终止工单#{}-{}".format(workflow_type_display, audit_id, workflow_title)
         # 接收人，发送给该资源组内对应权限组所有的用户
         auth_group_names = [Group.objects.get(id=auth_group_id).name for auth_group_id in
                             audit_detail.audit_auth_groups.split(',')]
@@ -189,8 +189,8 @@ def notify_for_execute(workflow, **kwargs):
     audit_auth_group, current_audit_auth_group = Audit.review_info(workflow.id, 2)
     audit_id = Audit.detail_by_workflow_id(workflow.id, 2).audit_id
     url = "{base_url}/workflow/{audit_id}".format(base_url=base_url, audit_id=audit_id)
-    msg_title = "[{}]工单{}#{}".format(WorkflowDict.workflow_type['sqlreview_display'],
-                                     workflow.get_status_display(), audit_id)
+    msg_title = "[{}]工单{}#{}-{}".format(WorkflowDict.workflow_type['sqlreview_display'],
+                                     workflow.get_status_display(), audit_id, workflow.workflow_name)
     msg_content = '''发起人：{}\n组：{}\n审批流程：{}\n工单名称：{}\n工单地址：{}\n工单详情预览：{}\n'''.format(
         workflow.engineer_display,
         workflow.group_name,
@@ -234,7 +234,7 @@ def notify_for_execute(workflow, **kwargs):
         # 判断上线语句是否存在DDL，存在则通知相关人员
         if workflow.syntax_type == 1:
             # 消息内容通知
-            msg_title = '[Archery]有新的DDL语句执行完成#{}'.format(audit_id)
+            msg_title = '[Archery]有新的DDL语句执行完成#{}-{}'.format(audit_id, workflow.workflow_name)
             msg_content = '''发起人：{}\n变更组：{}\n变更实例：{}\n变更数据库：{}\n工单名称：{}\n工单地址：{}\n工单预览：{}\n'''.format(
                 Users.objects.get(username=workflow.engineer).display,
                 workflow.group_name,
